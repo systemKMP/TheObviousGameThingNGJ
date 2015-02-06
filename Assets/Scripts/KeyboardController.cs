cc36fb4390dd3e8080e2ee037df5ee6280e092d2
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2015-02-06 23:24:01 +0100

    Movement works

---
+++ Assets/Scripts/KeyboardController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardController : MonoBehaviour
+{
+    public PlayerMovement Player;
+    public string WalkInputAxis;
+    public KeyCode JumpInput;
+
+    public void Update()
+    {
+        if(Player == null) { Debug.LogWarning("You haven't attached a player to me, moron"); return; }
+
+        Player.Walk(Input.GetAxis(WalkInputAxis));
+        if(Input.GetKeyDown(JumpInput)) Player.Jump();
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,52 +10,44 @@ public class PlayerMovement : MonoBehaviour
     public float WalkFloatiness;
     public float JumpForce;
 
-    public Direction _direction;
+    private float _direction;
+    private float _prewalk;
     private float _walk;
     private float _time;
+    private float _jump;
 
     public void Update()
     {
-        if (_direction == Direction.None)
+        if (Mover == null) { Debug.LogWarning("You haven't attached a movement script to me, moron"); return; }
+
+        float desiredWalk = 0;
+        if (_direction != 0)
         {
-            if (_time > 0)
-            {
-                _walk = WalkCurve.Evaluate(1 - _time / WalkFloatiness) * (_direction == Direction.Left ? -1 : 1) * MaxWalkSpeed;
-                _time -= Time.deltaTime;
-            }
-            else
-            {
-                _walk = 0;
-            }
+            desiredWalk = Mathf.Clamp(_direction, -MaxWalkSpeed, MaxWalkSpeed) * MaxWalkSpeed;
+        }
+        /*if (_time < WalkFloatiness) TODO I'll fix this part later, rasmus
+        {
+            _walk = Mathf.Lerp(_prewalk, desiredWalk, WalkCurve.Evaluate(_time / WalkFloatiness));
+            _time += Time.deltaTime;
         }
         else
         {
-            if (_time <= WalkFloatiness)
-            {
-                _walk = WalkCurve.Evaluate(_time / WalkFloatiness) * (_direction == Direction.Left ? -1 : 1) * MaxWalkSpeed;
-                _time += Time.deltaTime;
-            }
-            else
-            {
-                _walk = (_direction == Direction.Left ? -1 : 1) * MaxWalkSpeed;
-            }
-        }
+            _walk = desiredWalk;
+        }*/
+        _walk = desiredWalk;
 
-        Mover.Move = new Vector2(_walk, 0);
+        Mover.Move = new Vector2(_walk, _jump);
+        _jump = 0;
     }
 
-    public void Walk(Direction direction)
+    public void Walk(float direction)
     {
+        if (_direction != direction) { _time = 0; _prewalk = _walk; }
         _direction = direction;
     }
 
     public void Jump()
     {
-        
+        _jump = JumpForce;
     }
-}
-
-public enum Direction
-{
-    Left, Right, None,
 }
