b097f9b172b0a3f7ce8b29c95e4ce05113ee2500
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2015-02-07 00:10:11 +0100

    Shooting and stuffs

--- Assets/Scripts/Controls/ControllerController.cs
+++ Assets/Scripts/Controls/ControllerController.cs
@@ -4,12 +4,22 @@ using System.Collections;
 public class ControllerController : MonoBehaviour
 {
     public PlayerMovement Player;
-    [Range(1,8)]
+    [Range(1, 8)]
     public int Controller = 1;
+
+    private float _presign = 0;
 
     public void Update()
     {
-        Player.Walk(Input.GetAxis("Joystick_"+Controller+"_Left_x"));
-        if (Input.GetKey("joystick " + (Controller) + " button 0")) Player.Jump();
+        // Walk
+        Player.Walk(Input.GetAxis("Joystick_" + Controller + "_Left_x"));
+
+        // Jump
+        if (Input.GetKey("joystick " + Controller + " button 0")) Player.Jump();
+
+        // Shoot
+        var sign = Mathf.Sign(Input.GetAxis("Joystick_" + Controller + "_Trigger"));
+        if (sign != _presign && sign == -1) Debug.Log("BANG!"); // TODO shoot
+        _presign = sign;
     }
 }
