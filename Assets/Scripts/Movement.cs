c2fcc62b30ac89ce96c049da1b44213fb2ebf32d
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2015-02-06 22:38:30 +0100

    A script that moves stuff

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public Vector2 Move = Vector3.zero;
+
+    private Rigidbody2D _rigidbody2D;
+
+    public virtual void Awake()
+    {
+        _rigidbody2D = rigidbody2D;
+    }
+
+    public virtual void FixedUpdate()
+    {
+        if (Move.x != 0)
+            _rigidbody2D.AddForce(new Vector2(Move.x - _rigidbody2D.velocity.x * _rigidbody2D.mass, 0), ForceMode2D.Impulse);
+        if (Move.y != 0)
+            _rigidbody2D.AddForce(new Vector2(0, Move.y - _rigidbody2D.velocity.y * _rigidbody2D.mass), ForceMode2D.Impulse);
+    }
+}
