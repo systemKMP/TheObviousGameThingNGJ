0b719a92153df88a1bcfff44376ab2d40cbd31ac
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2015-02-07 03:58:05 +0100

    Sprite animation basics

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlayerAnimator : MonoBehaviour
+{
+    public Dictionary<string, Sprite> Sprites = new Dictionary<string, Sprite>();
+    private SpriteRenderer _renderer;
+
+    public void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void ChangeState(string spriteName)
+    {
+        _renderer.sprite = Sprites[spriteName];
+    }
+}
