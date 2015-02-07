73ca286afa3a4d6f2086cdd591bb681e9bee7057
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2015-02-07 02:00:02 +0100

    Weapon Spawning

---
+++ Assets/Editor/WeaponSpawnerEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(WeaponSpawner))]
+public class WeaponSpawnerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var spawner = (WeaponSpawner)target;
+
+        GUILayout.BeginVertical();
+        for(int i = 0; i < spawner.Weapons.Count; i++)
+        {
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Delete"))
+            {
+                spawner.Weapons.RemoveAt(i);
+                spawner.Rates.RemoveAt(i);
+                GUILayout.EndHorizontal();
+                break;
+            }
+            spawner.Weapons[i] = (Weapon)EditorGUILayout.ObjectField("", spawner.Weapons[i], typeof(Weapon), false);
+            spawner.Rates[i] = EditorGUILayout.FloatField("", spawner.Rates[i]);
+            GUILayout.EndHorizontal();
+        }
+        GUILayout.BeginHorizontal();
+        var newWeapon = (Weapon)EditorGUILayout.ObjectField("New Weapon:", null, typeof(Weapon), false);
+        GUILayout.EndHorizontal();
+        GUILayout.Space(5);
+        GUILayout.BeginHorizontal();
+        spawner.SpawnMin = EditorGUILayout.FloatField("", spawner.SpawnMin);
+        spawner.SpawnMax = EditorGUILayout.FloatField("", spawner.SpawnMax);
+        GUILayout.EndHorizontal();
+        if (newWeapon != null)
+        {
+            spawner.Weapons.Add(newWeapon);
+            spawner.Rates.Add(1);
+        }
+        GUILayout.EndVertical();
+    }
+}
---
+++ Assets/Scripts/WeaponSpawner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+public class WeaponSpawner : MonoBehaviour
+{
+    public List<Weapon> Weapons = new List<Weapon>();
+    public List<float> Rates = new List<float>();
+    public float SpawnMin = 2;
+    public float SpawnMax = 4;
+
+    private float _time;
+    private Weapon _currentWeapon;
+
+    public void Update()
+    {
+        if (CanSpawn())
+        {
+            if (_time > 0)
+            {
+                _time -= Time.deltaTime;
+            }
+            else
+            {
+                _time = Random.Range(SpawnMin, SpawnMax);
+
+                var total = Rates.Sum();
+                var result = Random.Range(0, total);
+                var accum = 0f;
+                for (int i = 0; i < Rates.Count; i++)
+                {
+                    accum += Rates[i];
+                    if (result < accum)
+                    {
+                        _currentWeapon = (Weapon) Instantiate(Weapons[i], transform.position, transform.rotation);
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (_currentWeapon == null || parentIsPlayer(_currentWeapon))
+                _currentWeapon = null;
+        }
+    }
+
+    private bool parentIsPlayer(Weapon weapon)
+    {
+        if (weapon == null || weapon.transform == null || weapon.transform.parent == null) return false;
+        return weapon.transform.parent.GetComponent<PlayerMovement>() != null;
+    }
+
+    private bool CanSpawn()
+    {
+        return _currentWeapon == null;
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 1);
+    }
+}
