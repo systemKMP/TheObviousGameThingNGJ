56f3cfbb0b7fe15807f286c3645661f9436420f5
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2015-02-07 01:19:09 +0100

    Auto calibrate dead zone

--- Assets/Scripts/Controls/ControllerController.cs
+++ Assets/Scripts/Controls/ControllerController.cs
@@ -4,30 +4,50 @@ using System.Collections;
 public class ControllerController : MonoBehaviour
 {
     public PlayerMovement Player;
-    [Range(1, 8)]
-    public int Controller = 1;
 
-    private float _presign = 0;
-
-    PlayerCore core;
-
-    void Start()
+    public int Controller
     {
-        core = gameObject.GetComponent<PlayerCore>();
+        get { return _controller; }
+        set
+        {
+            _controller = value;
+            _calibrated = false;
+        }
+    }
+
+    private bool _calibrated = true;
+    private float _dead;
+    private PlayerCore _core;
+    private int _controller;
+
+    public void Start()
+    {
+        _core = gameObject.GetComponent<PlayerCore>();
     }
 
     public void Update()
     {
+        if (!_calibrated)
+        {
+            var x = Input.GetAxis("Joystick_" + Controller + "_Left_x");
+            var y = Input.GetAxis("Joystick_" + Controller + "_Left_y");
+            var d = Mathf.Sqrt(x * x + y * y);
+            if (d < 0.5f)
+            {
+                _dead = d * 1.5f;
+                _calibrated = true;
+            }
+        }
+
         // Walk
-        Player.Walk(Input.GetAxis("Joystick_" + Controller + "_Left_x"));
+        var input = Input.GetAxis("Joystick_" + Controller + "_Left_x");
+        if (Mathf.Abs(input) < _dead) input = 0;
+        Player.Walk(input);
 
         // Jump
         if (Input.GetKeyDown("joystick " + Controller + " button 0")) Player.Jump();
 
         // Shoot
-        if (Input.GetAxis("Joystick_" + Controller + "_Trigger") < 0)
-        {
-            core.UseItems();
-        }
+        if (Input.GetAxis("Joystick_" + Controller + "_Trigger") < 0) _core.UseItems();
     }
 }
