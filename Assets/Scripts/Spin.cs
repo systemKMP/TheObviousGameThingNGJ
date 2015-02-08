e4060475dfcea0e41f38d6a32f5dc9c4062ba12f
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2015-02-08 02:14:07 +0100

    Stuff and things

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour
+{
+    public float GrowAmount;
+    public float TurnSpeed;
+    private Vector3 _scale;
+
+    public void Start()
+    {
+        _scale = transform.localScale;
+    }
+
+    public void Update()
+    {
+        transform.localScale = _scale + Vector3.one * Mathf.Abs(Mathf.Cos(Time.time * 2) * GrowAmount);
+        transform.Rotate(Vector3.forward, TurnSpeed*Time.deltaTime);
+    }
+}
