8250a6ec81ef00d4ed39f65cad019e8d31cebfa4
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2015-02-08 09:19:35 +0100

    Fixed it

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -10,12 +10,10 @@ public class CameraFollower : MonoBehaviour
     public float SpeedCompensation = 10;
     public Vector2 Bounds = Vector2.one;
     private Vector3 _target;
-    private Vector3 _origin;
-    private bool _viewToggle;
 
     public void Start()
     {
-        _origin = _target = transform.position;
+        _target = transform.position;
     }
 
     public void Update()
@@ -28,43 +26,13 @@ public class CameraFollower : MonoBehaviour
             transform.position = Vector3.Lerp(transform.position, _target, Lerp);
 
             var maxDist = players.Max(player => Vector2.Distance((player.transform.position + (Vector3)player.rigidbody2D.velocity*SpeedCompensation), transform.position));
-            var maxDistY = players.Max(player => Mathf.Abs((player.transform.position + (Vector3)player.rigidbody2D.velocity * SpeedCompensation).y - transform.position.y));
-
+            
             var cam = GetComponentInChildren<Camera>();
             if (cam != null)
             {
-                if (maxDistY > cam.orthographicSize) _viewToggle = true;
-                if (maxDistY < cam.orthographicSize*0.65f) _viewToggle = false;
-
                 cam.orthographicSize =
-                    Mathf.Lerp(cam.orthographicSize, Mathf.Min(_viewToggle ? float.PositiveInfinity : Bounds.x / (cam.aspect * 2), 
-                        maxDist + (players.Length == 1 ? Player1Margin : MorePlayerMargin)), Lerp);
-
-                if (!_viewToggle)
-                {
-                    if (_target.x < _origin.x - Bounds.x / 2 + cam.orthographicSize * cam.aspect)
-                    {
-                        _target.x = _origin.x - Bounds.x / 2 + cam.orthographicSize * cam.aspect;
-                    }
-                    if (transform.position.x > _origin.x + Bounds.x / 2 - cam.orthographicSize * cam.aspect)
-                    {
-                        _target.x = _origin.x + Bounds.x / 2 - cam.orthographicSize * cam.aspect;
-                    }
-                }
-                if (transform.position.y < _origin.y - Bounds.y / 2 + cam.orthographicSize)
-                {
-                    _target.y = _origin.y - Bounds.y / 2 + cam.orthographicSize;
-                }
-                if (transform.position.y > _origin.y + Bounds.y / 2 - cam.orthographicSize)
-                {
-                    _target.y = _origin.y + Bounds.y / 2 - cam.orthographicSize;
-                }
+                    Mathf.Lerp(cam.orthographicSize, maxDist + (players.Length == 1 ? Player1Margin : MorePlayerMargin), Lerp);
             }
         }
-    }
-
-    public void OnDrawGizmos()
-    {
-        Gizmos.DrawWireCube(transform.position, Bounds);
     }
 }
