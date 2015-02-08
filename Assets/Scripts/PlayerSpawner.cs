2ce5fff9392f5d5c3fad1f38fd63bbb60caf0a6b
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2015-02-08 01:13:36 +0100

    Priority spawn

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using System.Collections;
 
@@ -20,13 +21,18 @@ public class PlayerSpawner : MonoBehaviour
     public void Update()
     {
         if (_instance != this) return;
+        var players = GameObject.FindGameObjectsWithTag("Player");
+        var averageplayer = players.Aggregate(Vector3.zero, (pre, go) => pre + go.transform.position) / players.Length;
+        var maxDist = SpawnPoints.Max(spawner => Vector3.Distance(spawner.transform.position, averageplayer));
 
         for (int i = 1; i <= 8; i++)
         {
             if (Players.ContainsKey(i) && Players[i] == null) Players.Remove(i);
             if (Players.ContainsKey(i) || !Input.GetKey("joystick " + i + " button 2")) continue;
 
-            var player = (PlayerMovement)Instantiate(PlayerPrefab, SpawnPoints[Random.Range(0, SpawnPoints.Count)].transform.position,
+            var player = (PlayerMovement)Instantiate(PlayerPrefab, 
+                (SpawnPoints.SingleOrDefault(spawner => Vector3.Distance(spawner.transform.position, averageplayer) == maxDist)??
+                 SpawnPoints[Random.Range(0,SpawnPoints.Count)]).transform.position,
                 Quaternion.identity);
             var controller = player.GetComponent<JoystickController>() ??
                              player.gameObject.AddComponent<JoystickController>();
@@ -34,18 +40,18 @@ public class PlayerSpawner : MonoBehaviour
             controller.Index = i;
             Players[i] = player;
             Screenshaker.Shake(1, Vector2.up);
-            ScoreTracker.Instance.RegisterPlayer(i,player.GetComponent<PlayerCore>());
+            ScoreTracker.Instance.RegisterPlayer(i, player.GetComponent<PlayerCore>());
             GetComponent<AudioSource>().clip = SpawnClips[Random.Range(0, SpawnClips.Length)];
             GetComponent<AudioSource>().Play();
         }
-        
+
         if (Players.ContainsKey(0) && Players[0] == null) Players.Remove(0);
         if (!Players.ContainsKey(0) && Input.GetKey(KeyCode.Space))
         {
-            var keyboardplayer =
-                (PlayerMovement)
-                    Instantiate(PlayerPrefab, SpawnPoints[Random.Range(0, SpawnPoints.Count)].transform.position,
-                        Quaternion.identity);
+            var keyboardplayer = (PlayerMovement)Instantiate(PlayerPrefab,
+                (SpawnPoints.SingleOrDefault(spawner => Vector3.Distance(spawner.transform.position, averageplayer) == maxDist) ??
+                 SpawnPoints[Random.Range(0, SpawnPoints.Count)]).transform.position,
+                Quaternion.identity);
             var keyboardcontroller = keyboardplayer.GetComponent<KeyboardController>() ??
                                      keyboardplayer.gameObject.AddComponent<KeyboardController>();
             keyboardcontroller.Player = keyboardplayer;
