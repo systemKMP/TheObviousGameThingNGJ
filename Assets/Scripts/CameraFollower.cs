5e4ef93e38b8a46e8231b410450fc9ed457b5a63
systemKMP <km@gearworks.dk>
2015-02-08 19:38:21 +0100

    Camera pls

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -5,12 +5,22 @@ using System.Collections;
 public class CameraFollower : MonoBehaviour
 {
     public float Lerp = 0.3f;
-    public float Player1Margin = 10;
-    public float MorePlayerMargin = 4;
-    public float SpeedCompensation = 10;
+    public float PlayerMarginX = 20;
+    public float PlayerMarginY = 20;
+
     public Vector2 Bounds = Vector2.one;
     private Vector3 _target;
     public int minSize = 100;
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    public float horCamDif;
+    public float verCamDif;
+
+    public float targetOrth;
 
     public void Start()
     {
@@ -22,21 +32,40 @@ public class CameraFollower : MonoBehaviour
         var players = GameObject.FindGameObjectsWithTag("Player");
         if (players.Length > 0)
         {
-            var minX = players.Min(n => n.transform.position.x);
-            var maxX = players.Max(n => n.transform.position.x);
-            var minY = players.Min(n => n.transform.position.y);
-            var maxY = players.Max(n => n.transform.position.y);
-            var average = //players.Aggregate(Vector3.zero, (prev, player) => prev + player.transform.position) / players.Length;
-            _target = Vector3.Lerp(_target, new Vector3((minX + maxX) / 2.0f, (minY + maxY) / 2.0f, transform.position.z), Lerp);
-            transform.position = Vector3.Lerp(transform.position, _target, Lerp);
-
-            var maxDist = players.Max(player => Vector2.Distance((player.transform.position + (Vector3)player.rigidbody2D.velocity*SpeedCompensation), transform.position));
-            
             var cam = GetComponentInChildren<Camera>();
+
+            minX = players.Min(n => n.transform.position.x);
+            maxX = players.Max(n => n.transform.position.x);
+            minY = players.Min(n => n.transform.position.y);
+            maxY = players.Max(n => n.transform.position.y);
+
+            horCamDif = cam.orthographicSize * cam.aspect * 2;
+            verCamDif = cam.orthographicSize * 2;
+
+            targetOrth = minSize;
+
+            if ((maxX - minX) + PlayerMarginX < horCamDif)
+            {
+                targetOrth = ((maxX - minX) / cam.aspect) / 2.0f + PlayerMarginX;
+            }
+
+            if ((maxY - minY) + PlayerMarginY < verCamDif)
+            {
+                Debug.Log("vert fix");
+                targetOrth = (maxY - minY) / 2.0f + PlayerMarginY;
+            }
+
+            _target = Vector3.Lerp(_target, new Vector3((minX + maxX) / 2.0f, (minY + maxY) / 2.0f, transform.position.z), Lerp);
+
+            transform.position = Vector3.Lerp(transform.position, _target, Lerp * Time.deltaTime);
+
+            var maxDist = players.Max(player => Vector2.Distance((player.transform.position + (Vector3)player.rigidbody2D.velocity), transform.position));
+
+
             if (cam != null)
             {
                 cam.orthographicSize =
-                    Mathf.Lerp(cam.orthographicSize, maxDist/1.5f + (players.Length == 1 ? Player1Margin : MorePlayerMargin), Lerp);
+                    Mathf.Lerp(cam.orthographicSize, targetOrth, Time.deltaTime * Lerp);
                 cam.orthographicSize = minSize > cam.orthographicSize ? minSize : cam.orthographicSize;
             }
         }
