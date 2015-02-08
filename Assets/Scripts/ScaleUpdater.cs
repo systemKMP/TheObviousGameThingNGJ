a0c5b6846a5b89a0a78ee0aa63f3b5e5b6eccba8
systemKMP <km@gearworks.dk>
2015-02-08 03:33:27 +0100

    Map adjustment

---
+++ Assets/Scripts/ScaleUpdater.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleUpdater : MonoBehaviour {
+
+    public float size = 1.0f;
+
+    void Update()
+    {
+        if (Camera.current != null)
+        {
+            transform.localScale = Vector3.one * size * Camera.current.orthographicSize;
+        }
+    }
+}
