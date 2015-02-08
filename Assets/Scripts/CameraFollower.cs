e186c101e82ff31aabb0a91c85e2421841dd009c
systemKMP <km@gearworks.dk>
2015-02-08 13:48:24 +0100

    Final updates

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -21,8 +21,12 @@ public class CameraFollower : MonoBehaviour
         var players = GameObject.FindGameObjectsWithTag("Player");
         if (players.Length > 0)
         {
-            var average = players.Aggregate(Vector3.zero, (prev, player) => prev + player.transform.position) / players.Length;
-            _target = Vector3.Lerp(_target, new Vector3(average.x, average.y, transform.position.z), Lerp);
+            var minX = players.Min(n => n.transform.position.x);
+            var maxX = players.Max(n => n.transform.position.x);
+            var minY = players.Min(n => n.transform.position.y);
+            var maxY = players.Max(n => n.transform.position.y);
+            var average = //players.Aggregate(Vector3.zero, (prev, player) => prev + player.transform.position) / players.Length;
+            _target = Vector3.Lerp(_target, new Vector3((minX + maxX) / 2.0f, (minY + maxY) / 2.0f, transform.position.z), Lerp);
             transform.position = Vector3.Lerp(transform.position, _target, Lerp);
 
             var maxDist = players.Max(player => Vector2.Distance((player.transform.position + (Vector3)player.rigidbody2D.velocity*SpeedCompensation), transform.position));
@@ -31,7 +35,7 @@ public class CameraFollower : MonoBehaviour
             if (cam != null)
             {
                 cam.orthographicSize =
-                    Mathf.Lerp(cam.orthographicSize, maxDist + (players.Length == 1 ? Player1Margin : MorePlayerMargin), Lerp);
+                    Mathf.Lerp(cam.orthographicSize, maxDist/1.5f + (players.Length == 1 ? Player1Margin : MorePlayerMargin), Lerp);
             }
         }
     }
