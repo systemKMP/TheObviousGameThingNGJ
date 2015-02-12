77482b2f3c7a86443b50f69625228af5823ce5d7
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2015-02-12 18:23:18 +0100

    Fixed keyboard controls

--- Assets/Scripts/Controls/KeyboardController.cs
+++ Assets/Scripts/Controls/KeyboardController.cs
@@ -3,15 +3,16 @@ using System.Collections;
 
 public class KeyboardController : PlayerController
 {
-    public string WalkInputAxis = "Horizontal";
     public KeyCode JumpInput = KeyCode.W;
-    public KeyCode ShootInput = KeyCode.F;
+    public KeyCode RightInput = KeyCode.D;
+    public KeyCode LeftInput = KeyCode.A;
+    public KeyCode ShootInput = KeyCode.G;
 
     public void Update()
     {
         if(Player == null) { Debug.LogWarning("You haven't attached a player to me, moron"); return; }
 
-        Player.Walk(Input.GetAxis(WalkInputAxis));
+        Player.Walk((Input.GetKey(RightInput) ? 1 : 0) - (Input.GetKey(LeftInput) ? 1 : 0));
 
         if (Input.GetKeyDown(JumpInput)) Player.Jump();
         if (Input.GetKeyUp(JumpInput)) Player.StopJump();
