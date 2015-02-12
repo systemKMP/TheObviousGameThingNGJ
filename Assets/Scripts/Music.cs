241a9fd57958a770eb8803e2f38392b06fa30bf9
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2015-02-12 18:48:51 +0100

    Made the music not restart on stage change

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour
+{
+    private static Music _instance;
+
+    public void Start()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+            transform.parent = null;
+        }
+    }
+}
