71b7f4ec3ffa70a78f040673e6095c1e38de734d
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2015-02-12 18:39:34 +0100

    The keyboard can change maps now. Also other stuff

--- Assets/Scripts/PickLevel.cs
+++ Assets/Scripts/PickLevel.cs
@@ -4,30 +4,46 @@ using System.Collections;
 public class PickLevel : MonoBehaviour
 {
     public bool First;
-
+    public float CreditsTimer = 5;
+ 
     private int _level;
 
     public void Start()
     {
-        if (First)
-        {
-            _level = Random.Range(1, Application.levelCount);
-            Application.LoadLevel(_level);
-        }
         DontDestroyOnLoad(gameObject);
     }
 
     public void Update()
     {
+        if (CreditsTimer > 0)
+        {
+            CreditsTimer -= Time.deltaTime;
+            if (CreditsTimer <= 0)
+            {
+                if (First)
+                {
+                    _level = Random.Range(1, Application.levelCount);
+                    Application.LoadLevel(_level);
+                }
+            }
+            return;
+        }
+
         if (ScoreTracker.Instance.Scores.Count > 0) return;
 
-        for (int i = 1; i <= 4; i++)
+        for (int i = 0; i <= 4; i++)
         {
             var change = false;
-            var inputmax = Mathf.Max(Input.GetAxis("Joystick_" + i + "_Left_x"),
-                Input.GetAxis("Joystick_" + i + "_Left_y"));
-            var inputmin = Mathf.Min(Input.GetAxis("Joystick_" + i + "_Left_x"),
-                Input.GetAxis("Joystick_" + i + "_Left_y"));
+            var inputmax = i > 0 
+                ? Mathf.Max(Input.GetAxis("Joystick_" + i + "_Left_x"),
+                    Input.GetAxis("Joystick_" + i + "_Left_y"))
+                : Mathf.Max((Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0),
+                    (Input.GetKey(KeyCode.S) ? 1 : 0) - (Input.GetKey(KeyCode.W) ? 1 : 0));
+            var inputmin = i > 0
+                ? Mathf.Min(Input.GetAxis("Joystick_" + i + "_Left_x"),
+                    Input.GetAxis("Joystick_" + i + "_Left_y"))
+                : Mathf.Min((Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0),
+                    (Input.GetKey(KeyCode.S) ? 1 : 0) - (Input.GetKey(KeyCode.W) ? 1 : 0));
             if (inputmax >= 0.95f)
             {
                 _level++;
